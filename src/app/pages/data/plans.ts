0d68fd6f0befd4d3377c53b811452dcb8937aa73
Dario D. Müller <dario-domi@hotmail.de>
2019-05-02 20:02:45 +0200

    changed shop images and text

--- src/app/pages/data/plans.ts
+++ src/app/pages/data/plans.ts
@@ -3,9 +3,9 @@ import { Plan } from '../../models/plan';
 export const PLAN: Plan[] = [
     {
         id: 0,
-        image: "~/app/assets/images/1.png",
+        image: "~/app/assets/images/5.png",
         title: "Entry Magic",
-        content: "- 5 unlocked photo uploads\n- 0,30 € per upload",
+        content: "- unlock 5 photo uploads\n- 0,30 € per upload",
         plan: "once",
         cost: "1,50 €",
         isHighlight: true,
@@ -14,9 +14,9 @@ export const PLAN: Plan[] = [
     },
     {
         id: 1,
-        image: "~/app/assets/images/3.png",
+        image: "~/app/assets/images/10.png",
         title: "Advanced Magic",
-        content: "- 10 unlocked photo uploads\n- 0,25 € per upload",
+        content: "- unlock 10 photo uploads\n- 0,25 € per upload",
         plan: "once",
         cost: "2,50 €",
         isHighlight: false,
@@ -25,9 +25,9 @@ export const PLAN: Plan[] = [
     },
     {
         id: 2,
-        image: "~/app/assets/images/1.png",
+        image: "~/app/assets/images/20.png",
         title: "Professional Magic",
-        content: "- 20 unlocked photo uploads\n- 0,20 € per upload",
+        content: "- unlock 20 photo uploads\n- 0,20 € per upload",
         plan: "once",
         cost: "4,00 €",
         isHighlight: true,
@@ -36,7 +36,7 @@ export const PLAN: Plan[] = [
     },
     {
         id: 3,
-        image: "~/app/assets/images/3.png",
+        image: "~/app/assets/images/inf.png",
         title: "Unlimited Magic",
         content: "- Subscription period: 1 month\n- Unlimited photo uploads\n",
         plan: "abo",
@@ -47,7 +47,7 @@ export const PLAN: Plan[] = [
     },
     {
         id: 4,
-        image: "~/app/assets/images/3.png",
+        image: "~/app/assets/images/inf.png",
         title: "Unlimited Magic",
         content: "- Subscription period: 3 months\n- Unlimited photo uploads\n",
         plan: "abo",
