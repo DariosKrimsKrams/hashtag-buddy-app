73375749be6d6bbe72cfa3e9ff1d789cd5d91b8c
Dario D. Müller <dario-domi@hotmail.de>
2019-07-13 18:43:01 +0200

    fixed removed API for utils.ios.getter https://github.com/NativeScript/NativeScript/issues/7473

--- src/app/shared/status-bar-util.ts
+++ src/app/shared/status-bar-util.ts
@@ -12,7 +12,7 @@ export function setStatusBarColors() {
     if (application.ios) {
         let AppDelegate = UIResponder.extend({
             applicationDidFinishLaunchingWithOptions: function () {
-                utils.ios.getter(UIApplication, UIApplication.sharedApplication).statusBarStyle = UIStatusBarStyle.LightContent;
+                utils.ios['getter'](UIApplication, UIApplication.sharedApplication).statusBarStyle = UIStatusBarStyle.LightContent;
                 return true;
             }
         }, {
