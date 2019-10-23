3f5c8adfe8af20cf08f826b5509dd619b2d21549
Dario D. Müller <dario-domi@hotmail.de>
2019-10-23 21:07:34 +0200

    added ns-exit plugin and re-implemented it

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,9 +7,9 @@ import { SelectPhotoService } from '../../services/business-logic/select-photo.s
 import { UserService } from '~/app/storages/user.service';
 import { Photo } from '~/app/models/photo';
 import { Subscription } from 'rxjs';
-// import { exit } from 'nativescript-exit';
-// import * as Toast from 'nativescript-toast';
-// import { localize } from 'nativescript-localize/angular';
+import { exit } from 'nativescript-exit';
+import * as Toast from 'nativescript-toast';
+import { localize } from 'nativescript-localize/angular';
 
 @Component({
   selector: 'Home',
@@ -129,13 +129,12 @@ export class HomeComponent implements OnInit, OnDestroy {
       } else if (this.showConfirmImage) {
         this.showConfirmImage = false;
       } else {
-        // if (!this.backTriggeredForExit) {
-        //   this.backTriggeredForExit = true;
-        //   Toast.makeText(localize('exit_warning'), 'long').show();
-        // } else {
-        //   // exit();
-        //   // ToDo
-        // }
+        if (!this.backTriggeredForExit) {
+          this.backTriggeredForExit = true;
+          Toast.makeText(localize('exit_warning'), 'long').show();
+        } else {
+          exit();
+        }
       }
     }
   }
